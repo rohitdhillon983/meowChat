c78e9aad77472ce4f3edbda3c7df4bf76757c344
Rohit Dhillon <rohitdhillon983@gmail.com>
2024-12-25 14:47:57 +0530

    commit

--- MERN Chatapp/server/constants/config.js
+++ MERN Chatapp/server/constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
     origin: [
       "https://mern-meowchat.netlify.app",
-      "https://mern-meowchat.netlify.app/",
+      "http://localhost:5173",
       process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
